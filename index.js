88bf2cf7b963ac613a1b6ab7750ec8cfb2f2168b
Maverick Tesini <mavericktesini@gmail.com>
2018-08-18 06:20:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,7 +35,8 @@ function displayRepositories(dataItem) {
 
 function showCommits(dataItem) {
   // let comm_link = dataItem.commits_url.replace( /{\/sha}/ , '');
-  $.get(comm_link, function(comm_data) {
+  // $.get(comm_link, function(comm_data) {
+  $.get(dataItem.commits_url, function(comm_data) {
     console.log("pre comm data");
     console.log(comm_data);
     $('#details').append('Commits Found: ', comm_data.length);
